1c767a6728bf7fc4746a8dae99f59e41693e928a
SantePaulinum <SantePaulinum@localhost>
2010-10-26 23:13:29 +0400

    P2

---
+++ SimpleFormulaDrawer/Core/Definitions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public class DSet
+        {
+            private Dictionary<int, object> Arr;
+
+            public DSet(string What)
+            {
+                Arr = new Dictionary<int, object>();
+                if (What==null) {return;}
+                for (int i = 0; i < What.Length; i++)
+                {
+                    Arr.Add(i, What[i]);
+                }
+            }
+            public DSet(params object[] What)
+            {
+                Arr=new Dictionary<int,object>();
+                if (What == null) { return; }
+                for (int i = 0; i < What.Length; i++)
+                {
+                    Arr.Add(i, What[i]);
+                }
+            }
+            public bool Exists(object What)
+            {
+                return Arr.ContainsValue(What);
+            }
+            public void Add(object What)
+            {
+                if (!Arr.ContainsValue(What))
+                {
+                    Arr.Add(Arr.Count, What);
+                }
+            }
+            public void Remove(object What)
+            {
+                if (Arr.ContainsValue(What))
+                {
+                    for (int i = 0; i < Arr.Count; i++)
+                    {
+                        if (Object.Equals(Arr.ElementAt(i), What))
+                        {
+                            Arr.Remove(i);
+                            return;
+                        }
+                    }
+                }
+                return;
+            }
+        }
+        
+    public class TStateDescription
+        {
+            public int Code;
+            public string Description;
+            public TStateDescription(int Code, string Description)
+            {
+                this.Code = Code;
+                this.Description = Description;
+            }
+        }
+
+    public class TState
+        {
+            public TStateDescription C;
+            public TState N;
+            public TState(int Code, string Description)
+            {
+                this.C = new TStateDescription(Code, Description);
+            }
+        }
+}
