2177f78d4de4278fab88d2dde95794e2a72ec5a2
neofbi1 <neofbi1@gmail.com>
2010-10-27 19:59:11 +0400

    GraphForm and MainForm is creating

--- SimpleFormulaDrawer/interfac/DebugForm.xaml.cs
+++ SimpleFormulaDrawer/interfac/DebugForm.xaml.cs
@@ -24,8 +24,15 @@ namespace SimpleFormulaDrawer.interfac
         }
 
         public void AddMessage(string What)
-        {
+        {//метод добавления нового элемента. кэп.
             this.listBox1.Items.Add(What);
         }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {//Размеры текстбокса совпадают с размерами формы. кэп.
+            this.listBox1.Height=this.Height;
+            this.listBox1.Width=this.Width;
+        }
+
     }
 }
