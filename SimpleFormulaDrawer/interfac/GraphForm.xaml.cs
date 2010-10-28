2cf0355753e7e2e020d951fe40e7d2ed13403370
neofbi1 <neofbi1@gmail.com>
2010-10-28 17:31:38 +0400

    mainform,graphform

---
+++ SimpleFormulaDrawer/interfac/GraphForm.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleFormulaDrawer.interfac
+{
+    /// <summary>
+    /// Логика взаимодействия для GraphForm.xaml
+    /// </summary>
+    public partial class GraphForm : Window
+    {
+        public GraphForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.image1.Width = this.Width;
+            this.image1.Height = this.Height;
+        }
+    }
+}
---
+++ SimpleFormulaDrawer/interfac/MainForm.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms; //для скрина
+namespace SimpleFormulaDrawer.interfac
+{
+    /// <summary>
+    /// Логика взаимодействия для MainForm.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            int Wsize= System.Windows.Forms.Screen.AllScreens[0].WorkingArea.Width;//ширина 
+            int Hsize = System.Windows.Forms.Screen.AllScreens[0].WorkingArea.Height;//высота. надо.
+            this.Left = 5;
+            this.Top = 5;
+            this.Height = Hsize-this.Top;
+        }
+    }
+}
