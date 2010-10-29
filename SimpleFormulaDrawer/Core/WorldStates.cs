d6bacdd3f8187d45db8333991412148dbc44dd74
SantePaulinum <SantePaulinum@localhost>
2010-10-29 17:29:10 +0400

    Added button to check ConfigurationManager work. Applied some codestyle to LogManager. Remove VERY STRANGE files. Added minor improvements to ConfiguraionSystem.

---
+++ SimpleFormulaDrawer/Core/WorldStates.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace SimpleFormulaDrawer.Core
+{
+    static class WorldStates
+    {
+        private static TState SStart;
+
+        public static void AddState(int Code, string Description)
+        {
+            if (Code<0 && Code!=-2 && Code!=-100)
+            {
+                FlushStates();
+            }
+            TState CState;
+            if (SStart == null)
+            {
+                SStart = new TState(Code, Description);
+            }
+            else
+            {
+                CState = SStart;
+                while (CState != null)
+                {
+                    CState = CState.N;
+                }
+                CState = new TState(Code, Description);
+            }
+        }
+
+        public static TStateDescription GetState()
+        {
+            return SStart == null ? new TStateDescription(-1, "EMPTY") : SStart.C;
+        }
+
+        public static void CompleteState()
+        {
+            if (SStart != null)
+            {
+                SStart = SStart.N;
+            }
+        }
+
+        private static void FlushStates()
+        {
+            SStart = new TState(-2, "0");
+            GC.Collect();
+        }
+
+        public static string Serialize(Object[] What)
+        {
+            if (What == null)
+            {
+                return "";
+            }
+            var Res = "";
+            for (var i = 0; i<What.Length; i++)
+            {
+                if (What[i] != null) { Res += What[i].ToString(); }
+                if (i < What.Length - 1) { Res += ";"; }
+            }
+            return Res;
+        }
+
+        public static Object[] UnSerialize(string What, string SerializeMask)
+        {
+            var Res = new object[SerializeMask.Length];
+            var TMP = What.Split(';');
+            if (TMP.Length != SerializeMask.Length)
+            {
+                return null;
+            }
+            for (var i = 1; i < SerializeMask.Length; i++)
+            {
+                switch (SerializeMask[i])
+                {
+                    case 'i':
+                        {
+                            Res[i] = int.Parse(TMP[i]);
+                            break;
+                        }
+                    case 'f':
+                        {
+                            Res[i] = float.Parse(TMP[i]);
+                            break;
+                        }
+                    case 'b':
+                        {
+                            Res[i] = bool.Parse(TMP[i]);
+                            break;
+                        }
+                    case 's':
+                        {
+                            Res[i] = TMP[i];
+                            break;
+                        }
+                }
+            }
+            return Res;
+        }//UNSAFE
+
+        public static int GetStateParamsCount()
+        {
+            if (SStart==null) {return -1;}
+            return SStart.C.Description.Split(';').Length;
+        }
+    }
+}
---
+++ SimpleFormulaDrawer/interfac/MainForm.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+using TextBox = System.Windows.Controls.TextBox;
+
+//для скрина
+
+namespace SimpleFormulaDrawer.interfac
+{
+    /// <summary>
+    /// Логика взаимодействия для MainForm.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            var Wsize= Screen.AllScreens[0].WorkingArea.Width /5;//ширина 
+            var Hsize = Screen.AllScreens[0].WorkingArea.Height;//высота. надо.
+            Left = 5;
+            Top = 5;
+            Height = Hsize-Top;
+            Width = Wsize;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+  /*          this.progressBar1.Width = this.Width;
+            this.listBox1.Width = this.Width;
+            double heiff = (double) (this.Height/3);
+            heiff = Math.Round(heiff);
+            int H = (int) heiff;
+            this.listBox1.Height = H;*/
+          //  this.progressBar1.top=this.Top; 
+
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox) == null) return;
+            if ((sender as TextBox).Text == (sender as TextBox).Tag.ToString())
+            {
+                (sender as TextBox).Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox) == null) return;
+            if ((sender as TextBox).Text == "")
+            {
+                (sender as TextBox).Text = (sender as TextBox).Tag.ToString();
+            }
+        }
+    }
+}
