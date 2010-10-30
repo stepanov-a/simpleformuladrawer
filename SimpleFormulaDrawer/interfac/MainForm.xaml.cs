4afb8a8327ac62793de8bb0f6756c6dbe90562f9
neofbi1 <neofbi1@gmail.com>
2010-10-30 15:25:21 +0400

    Added AddNewGraph_Click simple Function, Interface was edited

---
+++ SimpleFormulaDrawer/interfac/MainForm.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+using TextBox = System.Windows.Controls.TextBox;
+
+//для скрина
+
+namespace SimpleFormulaDrawer.interfac
+{
+    /// <summary>
+    /// Логика взаимодействия для MainForm.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            var Wsize= Screen.AllScreens[0].WorkingArea.Width /5;//ширина 
+            var Hsize = Screen.AllScreens[0].WorkingArea.Height;//высота. надо.
+            Left = 5;
+            Top = 5;
+            Height = Hsize-Top;
+            Width = Wsize;
+        }
+
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox) == null) return;
+            if ((sender as TextBox).Text == (sender as TextBox).Tag.ToString())
+            {
+                (sender as TextBox).Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox) == null) return;
+            if ((sender as TextBox).Text == "")
+            {
+                (sender as TextBox).Text = (sender as TextBox).Tag.ToString();
+            }
+        }
+
+        private void AddFormul_Click(object sender, RoutedEventArgs e)
+        {
+            string str = this.FormulText.Text;
+            this.FormulListBox1.Items.Add(str);
+          
+        }
+
+        private void ButtNewGraphClick(object sender, RoutedEventArgs e)
+        {
+            GraphForm Graph = new GraphForm();
+            Graph.Top = this.Top;
+            Graph.Left = this.Left + this.Width+5; //позиция нвоой граф.формы слева =сумме начальной позиции  главной формы и ее ширины +5 -отступ
+            Graph.Height = this.Height; //высота графа равна высоте MainForm
+            Graph.Width = Screen.AllScreens[0].WorkingArea.Width - this.Width - this.Left-5;//Ширина Графа =ширине экрана за вычетом положения и размера главной формы
+            Graph.Show();
+        }
+    }
+}
