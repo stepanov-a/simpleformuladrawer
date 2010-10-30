1ca3c980b837749c99c1654262455497a4c8ec31
neofbi1 <neofbi1@gmail.com>
2010-10-30 16:27:45 +0400

    Added Fucking Animation (for test), Gardient BackGround In GraphForm, MainForm was edited

---
+++ SimpleFormulaDrawer/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SimpleFormulaDrawer.Core;
+using SimpleFormulaDrawer.interfac;
+
+namespace SimpleFormulaDrawer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var Main = new MainForm();
+            LogManager.Init("debug.log", "error.log");
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            Application.Current.MainWindow = Main;
+            Main.Show();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var LMGR=new LibraryManager();
+            LMGR.AddFunction("x^x+5SIN(x)-5^x");
+            LMGR.AddFunction("x*y");
+            LMGR.CompileSource();
+            textBlock1.Text = LMGR.GetSource();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            LogManager.WriteDebug("GAY");
+            Forms.DF.AddMessage("asas");
+            Forms.DF.Show();
+        }
+
+   
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(ConfigurationSystem.ReadConfig("CONFIG_FORM_THEME", "Default"));
+        }
+    }
+}
