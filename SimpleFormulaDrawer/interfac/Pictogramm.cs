3444592386aa028267005dd9f34fc6a803fddcb4
SantePaulinum <SantePaulinum@localhost>
2010-11-07 00:20:59 +0300

    I'm GAY.. REALLY..

--- SimpleFormulaDrawer/interfac/Pictogramm.cs
+++ SimpleFormulaDrawer/interfac/Pictogramm.cs
@@ -8,20 +8,11 @@ namespace SimpleFormulaDrawer.interfac
 
         private System.Windows.Controls.ListBox FornulList; //передается конструктором, но может быть изменен.
         public GraphForm GraphForm; //форма, с графиком
-        //диапазон построения
-        private Double xmin;
-        private double ymin;
-        private double xmax;
-        private double ymax;
-        //диапазон построения
 
         //constructor
-        public Pictogramm(Double xmin, double ymin, double xmax, double ymax, System.Windows.Controls.ListBox formullist)
+        public Pictogramm(ListBox formullist)
         { //конструктор забивает в форму диапазон построения. Если диапазон изменился-форма пересоздается (все равно все перерисовывать)
-            this.xmin = xmin;
-            this.xmax = xmax;
-            this.ymin = ymin;
-            this.ymax = ymax;
+
             this.FornulList = formullist;
             this.GraphForm = new GraphForm();
             this.GraphForm.Show();
