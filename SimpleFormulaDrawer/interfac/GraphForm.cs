1d874ffdf0ec1ead49b29acb96e40635f36dabd3
SantePaulinum <SantePaulinum@localhost>
2010-11-09 18:42:15 +0300

    Added 3DPoint struct. Started to write GraphForm.

--- SimpleFormulaDrawer/interfac/GraphForm.cs
+++ SimpleFormulaDrawer/interfac/GraphForm.cs
@@ -6,22 +6,37 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SimpleFormulaDrawer.Core;
 
 namespace SimpleFormulaDrawer.interfac
 {
     public partial class GraphForm : Form
     {
-        public GraphForm(double Minx,double Maxx,double Miny,double Maxy,int Quality)
-        {
-            InitializeComponent();
-        }
+        private bool Is3DRender;
 
-        private void GraphForm_Load(object sender, EventArgs e)
+        public GraphForm()
         {
             this.Top = 0;
             this.Left = Screen.PrimaryScreen.WorkingArea.Width/5;
             this.Width = Screen.PrimaryScreen.WorkingArea.Width/5*4;
             this.Height = Screen.PrimaryScreen.WorkingArea.Height;
         }
+
+        public void Set3DRendering(bool How)
+        {
+            if (Is3DRender == How) return;
+            Is3DRender = How;
+        }
+        public void Enable3DRendering()
+        {
+            if (Is3DRender) return;
+            Is3DRender = true;
+        }
+        public void Disable3DRendering()
+        {
+            if (!Is3DRender) return;
+            Is3DRender = false;
+        }
+
     }
 }
