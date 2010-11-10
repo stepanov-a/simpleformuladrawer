972b0433d0edcf1032416ddb75635257024bc27a
neofbi1 <neofbi1@gmail.com>
2010-11-10 14:05:11 +0300

    PictListBox selectColor is transparent now!

---
+++ SimpleFormulaDrawer/Core/Definitions.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using SimpleFormulaDrawer.interfac;
+using System.Windows.Shapes;
+using System.CodeDom.Compiler;
+using System.Drawing;
+
+namespace SimpleFormulaDrawer.Core
+{
+    public static class Forms
+    {
+        public static MainWindow StartupWindow;
+        public static readonly DebugForm DF = new DebugForm();
+    }
+
+    public class DSet
+        {
+            private readonly Dictionary<int, object> Arr;
+
+            public DSet(string What)
+            {
+                Arr = new Dictionary<int, object>();
+                if (What==null) {return;}
+                for (var i = 0; i < What.Length; i++)
+                {
+                    Arr.Add(i, What[i]);
+                }
+            }
+
+            public DSet(params object[] What)
+            {
+                Arr=new Dictionary<int,object>();
+                if (What == null) { return; }
+                for (var i = 0; i < What.Length; i++)
+                {
+                    Arr.Add(i, What[i]);
+                }
+            }
+
+            public bool Exists(object What)
+            {
+                return Arr.ContainsValue(What);
+            }
+            public void Add(object What)
+            {
+                if (!Arr.ContainsValue(What))
+                {
+                    Arr.Add(Arr.Count, What);
+                }
+            }
+            public void Remove(object What)
+            {
+                if (Arr.ContainsValue(What))
+                {
+                    for (var i = 0; i < Arr.Count; i++)
+                    {
+                        if (!Equals(Arr.ElementAt(i), What)) continue;
+                        Arr.Remove(i);
+                        return;
+                    }
+                }
+                return;
+            }
+        }
+        
+    public static class Crest
+    {
+        private readonly static Line[] ToRet=new Line[4];
+        private readonly static System.Windows.Media.Brush Strk=new SolidColorBrush(Colors.White);
+        private readonly static Line[] DCrest = new []
+                                                   {
+                                                       new Line {X1 = -10, X2 = -2, Y1 = 0, Y2 = 0, Stroke = Strk},
+                                                       new Line {X1 = 0, X2 = 0, Y1 = 10, Y2 = 2, Stroke = Strk},
+                                                       new Line {X1 = 2, X2 = 10, Y1 = 0, Y2 = 0, Stroke = Strk},
+                                                       new Line {X1 = 0, X2 = 0, Y1 = -2, Y2 = -10, Stroke = Strk}
+                                                   };
+        private static Line MoveLine(Line What,double x,double y)
+        {
+            return new Line { X1 = What.X1 + x, X2 = What.X2 + x, Y1 = What.Y1 + y, Y2 = What.Y2 + y, Stroke = Strk };
+        }
+
+        public static Line[] CrestToPoint(double x,double y)
+        {
+            ToRet[0] = MoveLine(DCrest[0],x,y);
+            ToRet[1] = MoveLine(DCrest[1], x, y);
+            ToRet[2] = MoveLine(DCrest[2], x, y);
+            ToRet[3] = MoveLine(DCrest[3], x, y);
+            return ToRet;
+        }
+    }
+
+    public struct FunctionParameters
+    {
+        public bool Is3D;
+        public CompilerErrorCollection Errors;
+    }
+
+    public struct Point3DF
+    {
+        public static readonly Point3DF Empty =new Point3DF();
+        private float x, y, z;
+
+        public Point3DF(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                return x == 0f && y == 0f && z == 0f;
+            }
+        }
+        public float X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public float Z
+        {
+            get
+            {
+                return z;
+            }
+            set
+            {
+                z = value;
+            }
+        }
+    }
+}
---
+++ SimpleFormulaDrawer/interfac/MainForm.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SimpleFormulaDrawer.Core;
+
+namespace SimpleFormulaDrawer.interfac
+{
+    /// <summary>
+    /// Логика взаимодействия для MainForm.xaml
+    /// </summary>
+    /// 
+    /// 
+    public partial class MainForm : Window
+    {
+        private List<Pictogramm> ArrPictogramm=new List<Pictogramm>();
+        private int CountPictogramm; //Нранит индекс последнго элемента в ArrPicrogramm
+        private int SelectedPictogram=0; //Текущая выбранная пиктограмма.
+        private double PictWidth;
+        private double PictHeight;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormulListBox1.Items.Clear();
+            this.CountPictogramm = 0;
+            this.FormulListBox1.Items.Clear();
+            this.PictHeight = Height / 25; //25-ЧИИСЛО НА ОДНОЙ СТРАНИЦЫ
+            //Расчитывается в конструкторе, чтобы не пересчитывать его, и не делать листбоксы из штук разного размера
+            AddPictogramm();
+        }
+
+
+        private void AddPictogramm() //добавляет новую пиктограмму в массив(лист) пиктограмм
+        {
+         //   MainGrid.ShowGridLines = true; //отображение линий грида. после отладки убрать.
+            this.ArrPictogramm.Add(new Pictogramm(-10, 10, -10, 10, 10));
+            this.CountPictogramm++;
+            Core.Forms.DF.AddMessage("Create New Pictogramm/graphform, his number is  "+CountPictogramm.ToString());//добавление в логи
+            ListBoxItem NewItem = new ListBoxItem();
+            NewItem.Content = this.ArrPictogramm[CountPictogramm - 1];
+            NewItem.Width = Double.NaN;
+            double ListHeight = this.PictlistBox.Height;
+      //      Core.Forms.DF.AddMessage(this.PictlistBox.Height.ToString()+"-PicrPistBox Height");
+            NewItem.Height = this.PictHeight;
+            this.PictlistBox.Items.Add(NewItem);
+            Boolean FullBar;
+            FullBar = (this.CountPictogramm > 23);
+            if (FullBar)
+            {
+                int ColIndex;
+                ColIndex = MainGrid.ColumnDefinitions.Count-1 ;
+                Core.Forms.DF.AddMessage(ColIndex.ToString()+"ColumnsDef-1");//добавление в логи
+                GridLengthConverter myGridLengthConverter = new GridLengthConverter();//Для управления шириной грида ее нужно конвертировать. танцы с бубном вокруг костра
+                MainGrid.ColumnDefinitions[ColIndex].Width = (GridLength)myGridLengthConverter.ConvertFromString("4*");
+              
+            }
+            
+         //   Core.Forms.DF.AddMessage(GridRow.ToString()+"-GridRow");
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            this.Left = 0;
+            this.Top = 0;
+            this.Height = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height;
+            this.Width =System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width/5;
+        }
+
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox) == null) return;
+            if ((sender as TextBox).Text == (sender as TextBox).Tag.ToString())
+            {
+                (sender as TextBox).Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox) == null) return;
+            if ((sender as TextBox).Text == "")
+            {
+                (sender as TextBox).Text = (sender as TextBox).Tag.ToString();
+            }
+        }
+
+        private void AddFormul_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem NewItem = new ListBoxItem
+                                       {
+                                           Foreground = new SolidColorBrush(ColorPicker1.InvertedSelectedColor),
+                                           Content = FormulText.Text,
+                                           Background = new SolidColorBrush(ColorPicker1.SelectedColor)
+                                       },
+                        TempNewItem = new ListBoxItem
+                                          {
+                                              Foreground = new SolidColorBrush(ColorPicker1.InvertedSelectedColor),
+                                              Content = FormulText.Text,
+                                              Background = new SolidColorBrush(ColorPicker1.SelectedColor)
+                                          };
+            if (NewItem.Content.ToString() == "Formula") return;
+
+            var Errors=this.ArrPictogramm[SelectedPictogram].AddFunction(NewItem);
+            if (Errors.Count==0)
+            {
+                this.FormulListBox1.Items.Add(TempNewItem);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка");
+                this.ArrPictogramm[SelectedPictogram].RemoveFunction(NewItem);
+                //Тут надо на самом деле подсвечивать начало чего то неправильного и показывать рядом тултип с описанием ошибки.
+            }
+        }
+
+        private void ButtNewGraphClick(object sender, RoutedEventArgs e)
+        {
+
+            AddPictogramm();
+          /*    GraphForm Graph = new GraphForm();
+            Graph.Top = this.Top;
+            Graph.Left = this.Left + this.Width + 5; //позиция нвоой граф.формы слева =сумме начальной позиции  главной формы и ее ширины +5 -отступ
+            Graph.Height = this.Height; //высота графа равна высоте MainForm
+            Graph.Width = Screen.AllScreens[0].WorkingArea.Width - this.Width - this.Left - 5;//Ширина Графа =ширине экрана за вычетом положения и размера главной формы
+            Graph.Show();*/
+        }
+
+        private void Debugbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.StartupWindow.Show();
+        }
+
+      
+    }
+}
