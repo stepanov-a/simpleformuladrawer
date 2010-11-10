0fefd0bbd82bc12c3262bbaa279dce2603133bc7
SantePaulinum <SantePaulinum@localhost>
2010-11-10 14:42:21 +0300

    GraphForms now opens and closes... opens and closes REALLY!!!!!!111!!!

---
+++ SimpleFormulaDrawer/interfac/DebugForm.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleFormulaDrawer.interfac
+{
+    /// <summary>
+    /// Логика взаимодействия для DebugForm.xaml
+    /// </summary>
+    public partial class DebugForm : Window
+    {
+        public DebugForm()
+        {
+            InitializeComponent();
+        }
+
+        public void AddMessage(string What, Color CLR)
+        {
+            var NewItem=new ListBoxItem
+                            {
+                                Content = What, Background = new SolidColorBrush(CLR)
+                            };
+            listBox1.Items.Add(NewItem);
+        }
+
+        public void AddMessage(string What)
+        {
+            listBox1.Items.Add(What);
+            listBox1.SelectedIndex = listBox1.Items.Count;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ SimpleFormulaDrawer/interfac/GraphForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SimpleFormulaDrawer.Core;
+
+namespace SimpleFormulaDrawer.interfac
+{
+    public partial class GraphForm : Form
+    {
+        private bool Is3DRender;
+        private Pictogramm Parent;
+
+        public GraphForm(Pictogramm Parent)
+        {
+            InitializeComponent();
+            this.Parent = Parent;
+        }
+
+        private void GraphForm_Load(object sender, EventArgs e)
+        {
+            this.Top = 0;
+            this.Left = Screen.PrimaryScreen.WorkingArea.Width / 5;
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width / 5 * 4;
+            this.Height = Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+        public void Set3DRendering(bool How)
+        {
+            if (Is3DRender == How) return;
+            Is3DRender = How;
+        }
+        public void Enable3DRendering()
+        {
+            if (Is3DRender) return;
+            Is3DRender = true;
+        }
+        public void Disable3DRendering()
+        {
+            if (!Is3DRender) return;
+            Is3DRender = false;
+        }
+
+        private void GraphForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Parent.ClosePictogramm();
+        }
+    }
+}
