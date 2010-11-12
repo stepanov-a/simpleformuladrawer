073d6193aa1fc7d5f81f6d787e91b6b450fa1ec8
SantePaulinum <SantePaulinum@localhost>
2010-11-13 01:39:30 +0300

    Added Z-Axis for GraphForm. Started drawing axis.

--- SimpleFormulaDrawer/interfac/GraphForm.cs
+++ SimpleFormulaDrawer/interfac/GraphForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -15,7 +16,9 @@ namespace SimpleFormulaDrawer.interfac
         private bool Is3DRender;
         private Pictogramm Parent;
         public int FormState = 0;
-
+        private Pen AxisColor = new Pen(Color.Black);
+        private Graphics GR;
+        public int Depth;//Глубина формы. Да, я не курил.
         public GraphForm(Pictogramm Parent)
         {
             InitializeComponent();
@@ -29,6 +32,8 @@ namespace SimpleFormulaDrawer.interfac
             this.Width = Screen.PrimaryScreen.WorkingArea.Width / 5 * 4;
             this.Height = Screen.PrimaryScreen.WorkingArea.Height;
             this.ShowInTaskbar = false;
+            this.BackColor = Color.White;
+            this.GR = this.CreateGraphics();
         }
 
         private void GraphForm_FormClosed(object sender, FormClosedEventArgs e)
@@ -42,7 +47,32 @@ namespace SimpleFormulaDrawer.interfac
             Is3DRender = How;
         }
         
-        
+        public void DrawAxis(params double [] AxisMarks)
+        {
+            /*Эта функция принимает 6 аргументов и обрабатывает 4 или 6 аргументов в зависимоти от флага IS3DRender.
+             * Порядок следования аргуменов:
+             * Xmin,Xmax,Ymin,YMax,Zmin,Zmax
+             */
+            float ZeroX = Shared.WhereZero(AxisMarks[0], AxisMarks[1], this.Width);
+            float ZeroY = Shared.WhereZero(AxisMarks[2], AxisMarks[3], this.Height);
+            float ZeroZ = Shared.WhereZero(AxisMarks[4], AxisMarks[5], this.Depth);
+            if ((Is3DRender && AxisMarks.Length<4) || (!Is3DRender && AxisMarks.Length<6)) throw new InvalidDataException();
+            if (Is3DRender)
+            {
+                
+            }
+            else
+            {
+                GR.DrawLine(AxisColor, 0, ZeroY, this.Width, ZeroY);
+                GR.DrawLine(AxisColor,ZeroX,0,ZeroX,this.Height);
+            }
+        }
+
+        private void GraphForm_Resize(object sender, EventArgs e)
+        {
+            GR = this.CreateGraphics();
+            this.Depth = (this.Width + this.Height)/2; //Я так сказал.
+        }
 
     }
 }
